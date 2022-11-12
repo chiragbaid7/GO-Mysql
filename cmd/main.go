e4dfa4bce8f04096a77c9bc1f766a964e775e154
Chirag Baid <chirag.baid@yellow.ai>
2022-11-12 16:04:53 +0530

    Add models path

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/chiragbaid7/GO-Mysql/pkg/models"
 	// "github.com/chiragbaid7/GO-Mysql/pkg/routes"
 	"github.com/gorilla/mux"
 )
@@ -14,7 +14,6 @@ func main() {
 		return new router instance that implements http.Handler interface
 		registers routes and controller/handler fn
 	*/
-	fmt.Println("chirag")
 	router := mux.NewRouter()
 	// routes.BooksStoreRoutes(router)
 	log.Fatal(http.ListenAndServe(":8080", router))
